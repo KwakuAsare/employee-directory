723b9cca3765c681aa86fbfd6870580805b6ceab
Kwaku Asare <lilkwaku55@yahoo.com>
2020-12-21 19:53:19 -0500

    redeploying

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import DirectoryPage from "./components/DirectoryPage";
 import Jumbotron from "./components/Jumbotron";
 
 function App() {
-  return <Jumbotron />;
+  return <div>
+    <Jumbotron />;
+    <DirectoryPage />
+  </div> 
 }
 
 export default App;
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -11,14 +11,6 @@ function Jumbotron() {
                     <button className="btn btn-primary btn-lg">Learn more</button>
                 </p>
             </div>
-            <div className="container">
-                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-                    <div className="col">Column</div>
-                    <div className="col">Column</div>
-                    <div className="col">Column</div>
-                    <div className="col">Column</div>
-                </div>
-            </div>
         </div>
     )
 }
